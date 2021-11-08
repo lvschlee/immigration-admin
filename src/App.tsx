aab6f396d5cda006e5c920e1b1c1cd7ebd29c14d
Alesia Sviridova <lvschlee@gmail.com>
2021-11-08 01:22:44 +0100

    feat(master): install antd, firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,61 @@
-import './App.css';
+import { initializeApp } from 'firebase/app';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+
+import { firebaseConfig } from './config';
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+type Post = {
+  title: string;
+}
 
 export function App() {
+  const [posts, loading] = useCollectionData(collection(db, 'posts'));
+
+  if (loading) {
+    return <p>loading</p>;
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Test</h1>
       </header>
+
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th>title</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {Array.isArray(posts)
+              ? posts.map((post) => (
+                  <tr key={post.title}>
+                    <td>{post.title}</td>
+                  </tr>
+                ))
+              : null}
+          </tbody>
+        </table>
+        <form action="">
+          <div>
+            <label htmlFor=""></label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor=""></label>
+            <input type="text" />
+          </div>
+          <div>
+            <input type="submit" value="создать" />
+          </div>
+        </form>
+      </section>
     </div>
   );
 }
