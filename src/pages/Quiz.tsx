da6f2c9d3f6c234aaebb2c3a237ec405e31dfc12
Alesia Sviridova <lvschlee@gmail.com>
2021-11-26 03:01:59 +0100

    feature(quiz): add quiz result in table

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,70 +1,165 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { v4 as uuid } from 'uuid';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { collection, doc, setDoc } from 'firebase/firestore';
-import {
-  Button,
-  Divider,
-  PageHeader,
-  Form,
-  Input,
-} from 'antd';
 
+import { Button, Divider, PageHeader, Modal, Table } from 'antd';
+
+import { PlusOutlined, DeleteOutlined, EditOutlined } from '@ant-design/icons';
+
+import { QuestionForm } from '../components/QuestionForm';
+import { observer } from 'mobx-react';
+
+import { useDocument } from '../hooks';
+import { useRootStore } from '../RootStateContext';
 
 import { db } from '../firebase';
 
-export function Quiz() {
+export const Quiz = observer(() => {
+  const { quizStore } = useRootStore();
   const [saving, setSaving] = useState(false);
-  const [quiz = [], loading] = useCollectionData<any, any, any>(collection(db, 'quiz'), {
+  const { remove: removeDocument, loading: isRemoveInProgress } = useDocument(
+    db,
+    'quiz'
+  );
+  const [questions, loading] = useCollectionData(collection(db, 'quiz'), {
     idField: 'id',
   });
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  useEffect(() => {
+    if (questions?.length) {
+      quizStore.addQuestions(questions as any);
+    }
+  }, [loading, questions, quizStore]);
+
+  const isEditMode = Boolean(quizStore.question);
+
+  const columns = [
+    {
+      title: 'Вопрос',
+      dataIndex: 'text',
+      key: 'text',
+    },
+    {
+      title: 'Ответы',
+      dataIndex: 'answers',
+      key: 'answers',
+      render(arr: any) {
+        return (
+          <ul>
+            {arr.map(({ text, next }: any) => (
+              <li key={text}>
+                {text} -&gt;{' '}
+                {next === 'finish'
+                  ? 'Завершение опроса'
+                  : questions?.find(({ id }) => id === next)?.text}
+              </li>
+            ))}
+          </ul>
+        );
+      },
+    },
+    {
+      title: 'Действия',
+      dataIndex: 'actions',
+      render: (text: string, record: any) => (
+        <div>
+          <Button
+            onClick={() => {
+              handleEdit(record.id);
+            }}
+          >
+            <EditOutlined />
+          </Button>
+          <Button
+            onClick={() => {
+              removeDocument(record.id);
+            }}
+          >
+            <DeleteOutlined />
+          </Button>
+        </div>
+      ),
+    },
+  ];
+
+  const handleEdit = (id: string) => {
+    quizStore.addQuestion(questions?.find(({ id: _id }) => _id === id) as any);
+    setIsModalVisible(true);
+  };
 
   const onFinish = async (values: any) => {
     try {
       setSaving(true);
+      const id = isEditMode ? quizStore?.question?.id ?? '' : uuid();
 
-      await setDoc(doc(db, 'quiz', 'main'), values);
-    } catch(e) {
+      await setDoc(doc(db, 'quiz', id), values);
+    } catch (e) {
       console.info(e);
     } finally {
       setSaving(false);
+      setIsModalVisible(false);
     }
   };
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    quizStore.addQuestion(undefined);
+    setIsModalVisible(false);
+  };
+
+  const modalTitle = isEditMode ? 'Редактирование вопроса' : 'Новый вопрос';
+  const okText = isEditMode ? 'сохранить' : 'создать';
 
   if (loading) {
     return <p>loading</p>;
   }
 
-  const { schema } = quiz[0];
-
   return (
-    <div>
-      <PageHeader
-        ghost={false}
-        onBack={() => window.history.back()}
-        title="Опросник"
-      />
-      <Divider />
-
+    <>
       <div>
-        <Form
-          name="basic"
-          id="myForm"
-          initialValues={{ remember: true, schema }}
-          layout="vertical"
-          onFinish={onFinish}
-          autoComplete="off"
-        >
-          <Form.Item
-            name="schema"
-            rules={[{ required: true, message: 'Поле не может быть пустым' }]}
-          >
-            <Input.TextArea autoSize={{ minRows: 20 }} />
-          </Form.Item>
-          <Form.Item>
-            <Button loading={saving} htmlType="submit" type="primary">Сохранить</Button>
-          </Form.Item>
-        </Form>
+        <PageHeader
+          ghost={false}
+          onBack={() => window.history.back()}
+          title="Опросник"
+          extra={[
+            <Button key="primary" type="primary" onClick={showModal}>
+              Добавить
+              <PlusOutlined />
+            </Button>,
+          ]}
+        />
+        <Divider />
+
+        <Table
+          columns={columns}
+          dataSource={questions}
+          loading={loading || isRemoveInProgress}
+        />
       </div>
-    </div>
+
+      <Modal
+        centered
+        cancelText="отменить"
+        destroyOnClose={true}
+        title={modalTitle}
+        okText={okText}
+        visible={isModalVisible}
+        onCancel={handleCancel}
+        afterClose={handleCancel}
+        okButtonProps={{
+          form: 'myForm',
+          htmlType: 'submit',
+          disabled: saving,
+        }}
+      >
+        <QuestionForm onFinish={onFinish} />
+      </Modal>
+    </>
   );
-}
+});
